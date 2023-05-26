cb6fdd4cb5f11cb96faeeb046de06df6e1f1a71f
Remcycling <22045554@myrp.edu.sg>
2023-05-26 12:28:41 +0800

    first push to P00 repository

--- P00/test/CalculatorTest.java
+++ P00/test/CalculatorTest.java
@@ -1,51 +1,51 @@
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
-class CalculatorTest {
+public class CalculatorTest {
 
-	@BeforeAll
-	static void setUpBeforeClass() throws Exception {
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
 	}
 
-	@AfterAll
-	static void tearDownAfterClass() throws Exception {
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
 	}
 
-	@BeforeEach
-	void setUp() throws Exception {
+	@Before
+	public void setUp() throws Exception {
 	}
 
-	@AfterEach
-	void tearDown() throws Exception {
+	@After
+	public void tearDown() throws Exception {
 	}
 
 	@Test
 	public void testAdd() {
-		int a = 4321;
-		int b = 1234;
-			
-		Calculator cal = new Calculator();
-		int actual = cal.add(a, b); 
-			 
-		int expected = 5555;
-		assertEquals (expected, actual);
-		}
+			int a = 4321;
+			int b = 1234;
+				
+			Calculator cal = new Calculator();
+			int actual = cal.add(a, b); 
+				 
+			int expected = 5555;
+			assertEquals (expected, actual);
+			}
 	@Test
 	public void testSubtract() {
-		int a = 9876;
-		int b = 4321;
-			
-		Calculator cal = new Calculator();
-		int actual = cal.subtract(a, b); 
-			 
-		int expected = 5555;
-		assertEquals (actual, expected);
-		}
+			int a = 9876;
+			int b = 4321;
+				
+			Calculator cal = new Calculator();
+			int actual = cal.subtract(a, b); 
+				 
+			int expected = 5555;
+			assertEquals (actual, expected);
+			}
+	}
 
 
-}
